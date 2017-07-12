c57b87668f5793317848b24f18514897d10719ab
Simon Peter Ssekamatte <spssekamatte@gmail.com>
2017-07-12 18:45:38 +0300

    Add new background image
    Add image file named landscape.jpg to the background and edit .travics.yml
    file to  include coveralls installation as well as requirements
    installation

--- app/views.py
+++ app/views.py
@@ -1,5 +1,9 @@
-from flask import Flask, flash, redirect, render_template, request, session, abort
+from flask import flash, render_template, request, session
 from app import app
+from datetime import datetime
+from app.models.user import User
+
+users = {}
 
 @app.route('/')
 def home():
@@ -9,15 +13,46 @@ def home():
         return render_template("home.html")
 
 
-@app.route('/login', methods=['GET','POST'])
+@app.route('/login', methods=['POST'])
 def login():
-    if request.form['password'] == 'duff' and request.form['username'] == 'homer':
+    if request.form['username'] in users.keys() and users[request.form['username']] == request.form['password']:
         session['logged_in'] = True
     else:
         flash('Invalid username or password!')
     return home()
 
 
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+    return home()
+
+
 @app.route('/signup')
-def sign_up():
+def sign_up_page():
     return render_template("sign_up.html")
+
+
+@app.route('/signup', methods=['POST'])
+def sign_up():
+    the_form = request.form
+    if the_form:
+        last_name = ""
+        name = the_form['name']
+        names = name.split()
+        if len(names) > 1:
+            last_name = names[len(names)-1]
+        first_name = names[0]
+        new_user = User(
+            username=the_form['username'],
+            password=the_form['password'],
+            first_name=first_name,
+            last_name=last_name,
+            birthday=datetime.strptime(str(the_form['birthday']), '%Y-%m-%d')
+        )
+        users[new_user.username] = new_user
+        if new_user.username in users.keys():
+            session['logged_in'] = True
+            return home()
+    else:
+        return sign_up_page()
